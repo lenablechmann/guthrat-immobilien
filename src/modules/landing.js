cc4fb780398072d80d776bb673097d1eb5563b98
Lena Blechmann <lenablechmann@gmail.com>
2021-04-17 16:06:26 +0200

    create another html element, need to figure out dynamic

--- src/modules/landing.js
+++ src/modules/landing.js
@@ -1,8 +1,11 @@
 // creates landing page in the index html
 export function loadLandingPage() {
-  console.log("supposedly this module got exported, nice.");
-    const element = document.createElement('nav');
-    element.innerHTML = "testing dom modification";
-    element.classList.add('top-nav')
-    document.body.appendChild(element);
+  const topNav = document.createElement('nav');
+  topNav.classList.add('top-nav');
+  document.body.appendChild(topNav);
+
+  const topNavInfo = document.createElement('div');
+  topNavInfo.classList.add('top-nav--upper-info');
+  document.topNav.appendChild(topNavInfo);
+
 }
