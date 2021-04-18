a36ac969c4bf772c7c3edf1edff6f136a8a4a571
Lena Blechmann <lenablechmann@gmail.com>
2021-04-18 15:56:43 +0200

    make nested object for nested div dom creation

--- src/modules/landing.js
+++ src/modules/landing.js
@@ -1,5 +1,43 @@
 // creates landing page in the index html
 export function loadLandingPage() {
+  const TopNavEl = 
+    {
+    tag: 'nav',
+    class: 'top-nav',
+    children: [
+      {
+        tag: 'div',
+        class: 'top-nav--upper-info',
+        text: '',
+        children: [
+          {
+            tag: 'div',
+            class: 'upper-info--sale-pitch',
+            text: 'IHR IMMOBILIENMAKLER IN KÖLN',
+            children: []
+          },
+          {
+            tag: 'div',
+            class: 'upper-info--tel',
+            text: 'Tel.: 0221-444-5566',
+            children: []
+          },
+          {
+            tag: 'div',
+            class: 'upper-info--mail',
+            text: 'E-Mail: guthrat@immobilien.de',
+            children: []
+          },
+      ],
+      },
+      {tag: 'ul',
+      class: 'top-nav--menu',
+      text: '',
+      children: [],
+      },
+    ],
+
+  };
   const topNav = document.createElement('nav');
   topNav.classList.add('top-nav');
   document.body.appendChild(topNav);
