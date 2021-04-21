ae14efec7ac3782404902064f0fd2c19403243ed
Lena Blechmann <lenablechmann@gmail.com>
2021-04-21 13:33:23 +0200

    append upper nav to the dist html

--- src/modules/landing.js
+++ src/modules/landing.js
@@ -1,26 +1,35 @@
 // creates landing page in the index html
 export function loadLandingPage() {
-  class elementHTML {
+
+  class HTMLelement {
     constructor(tag,className, innerHTML) {
         let element = document.createElement(tag);
         element.className = className;
-        element.innerHTML = innerHTML;
+        if (innerHTML){
+          element.innerHTML = innerHTML;
+        }
 
         return element;
     }
 }
-let innerHTML = "testing this thang"
-const testing = new elementHTML('div', 'section-header', innerHTML);
-document.body.appendChild(testing)
 
-  // function topNav () {
-  //   const topNav = document.createElement('nav');
-  //   topNav.classList.add('top-nav');
-  //   document.body.appendChild(topNav);
+// html creation, using IIFE pattern for keeping namespace clean 
+// and separate blocks of html
+(function createTopNav () {
+  const topNav = new HTMLelement ('nav', 'top-nav');
+  document.body.appendChild(topNav);
 
-  //   const topNavInfo = document.createElement('div');
-  //   topNavInfo.classList.add('top-nav--upper-info');
-  //   document.topNav.appendChild(topNavInfo);
-  // }
+  const topNavUpper = new HTMLelement ('div', 'top-nav--upper-info');
+  topNav.appendChild(topNavUpper);
+
+  const salePitch = new HTMLelement ('div', 'upper-info--sale-pitch', 'IHR IMMOBILIENMAKLER IN KÖLN');
+  topNavUpper.appendChild(salePitch);
+
+  const infoTel = new HTMLelement ('div', 'upper-info--tel', 'Tel.: 0221-444-5566');
+  topNavUpper.appendChild(infoTel);
+
+  const infoMail = new HTMLelement ('div', 'upper-info--mail', 'E-Mail: guthrat@immobilien.de');
+  topNavUpper.appendChild(infoMail);
+})();
 
 }
